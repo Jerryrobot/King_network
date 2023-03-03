70e7933b6367ac1d4453595378f6c754eaee7809
Jerryrobot <125436508+Jerryrobot@users.noreply.github.com>
2023-03-03 11:10:18 +0530

    Update config.py
    Signed-off-by: Jerryrobot <125436508+Jerryrobot@users.noreply.github.com>

--- config.py
+++ config.py
@@ -1,10 +1,10 @@
 """Config fill properly"""
 import os
 
-API_ID = int(os.environ.get("API_ID", "6435225"))
-API_HASH = os.environ.get("API_HASH", "4e984ea35f854762dcde906dce426c2d")
-BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
-MONGO_DB_URI = os.environ.get("MONGO_DB_URI", "")
+API_ID = int(os.environ.get("API_ID", "20729355"))
+API_HASH = os.environ.get("API_HASH", "3559855a79208a95323343c56b142220")
+BOT_TOKEN = os.environ.get("BOT_TOKEN", "6096307685:AAFQ_96C21XeW0bU3PBwN8oai8n56JF8OqQ")
+MONGO_DB_URI = os.environ.get("MONGO_DB_URI", "mongodb+srv://Aamirjemadar01:aamir01@cluster0.uyclyiz.mongodb.net/?retryWrites=true&w=majority")
 DB_URI = os.environ.get("ELEPHANT_SQL", "")
-OWNER_ID = int(os.environ.get("OWNER_ID", ""))
-BOT_ID = int(os.environ.get("BOT_ID", ""))
+OWNER_ID = int(os.environ.get("OWNER_ID", "5864436910"))
+BOT_ID = int(os.environ.get("BOT_ID", "-1001694132088"))
