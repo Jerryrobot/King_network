9505678ed28ed9c7baead75bb3cc739e273b464f
Jerryrobot <125436508+Jerryrobot@users.noreply.github.com>
2023-03-08 15:19:09 +0530

    Update config.py
    Signed-off-by: Jerryrobot <125436508+Jerryrobot@users.noreply.github.com>

--- config.py
+++ config.py
@@ -3,7 +3,7 @@ import os
 
 API_ID = int(os.environ.get("API_ID", "10704655"))
 API_HASH = os.environ.get("API_HASH", "45aa4245259c66ecab638755b406f0a0")
-BOT_TOKEN = os.environ.get("BOT_TOKEN", "6284396856:AAEs4RM3cUOs6qxsfHey4EmVX2I-ZJl2HSo")
+BOT_TOKEN = os.environ.get("BOT_TOKEN", "6190039100:AAFRHuOPumSCpb8nb-gxZ_wmD6hKRRqBepg")
 MONGO_DB_URI = os.environ.get("MONGO_DB_URI", "mongodb+srv://Aamirjemadar01:aamir01@cluster0.uyclyiz.mongodb.net/?retryWrites=true&w=majority")
 DB_URI = os.environ.get("ELEPHANT_SQL", "postgres://jcnfgwip:R2Y9NPWvALAp90UHd1kwPbt6f2TsSzOd@babar.db.elephantsql.com/jcnfgwip")
 OWNER_ID = int(os.environ.get("OWNER_ID", "5864436910"))
